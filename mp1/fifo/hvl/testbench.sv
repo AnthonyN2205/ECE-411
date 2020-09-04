787eea276e266ee59a15e0b0f8e9b508b7390b6c
Anthony <alnguyn2@illinois.edu>
2020-09-04 12:40:43 -0500

    fifo done

--- mp1/fifo/hvl/testbench.sv
+++ mp1/fifo/hvl/testbench.sv
@@ -36,12 +36,115 @@ endfunction : report_error
 
 // DO NOT MODIFY CODE ABOVE THIS LINE
 
+
+
+/* enqueue element */
+task enqueue(int i);
+    @(tb_clk);
+    /* if queue is full */
+    if (itf.rdy == 1'b0) begin
+        itf.valid_i <= 1'b0;
+        return;
+    end
+
+    /* valid-ready protocol */
+    itf.valid_i <= 1'b1;
+
+    /* enqueue data */
+    itf.data_i <= i;
+
+    $display("%d added", i);
+    $display ("--------------");
+
+    @(tb_clk);
+    itf.valid_i <= 1'b0;
+       
+endtask : enqueue
+
+
+
+/* dequeue element */
+task dequeue(int j);
+    @(tb_clk);
+
+    if (itf.valid_o == 1'b0) begin
+        itf.yumi <= 1'b0;
+        return;
+    end
+
+    itf.yumi <= 1'b1;
+    assert(itf.data_o == j)
+    else begin
+        $error ("%0d: %0t: INCORRECT_DATA_O_ON_YUMI_I error detected", `__LINE__, $time);
+        report_error (INCORRECT_DATA_O_ON_YUMI_I);
+    end 
+
+    $display("%d removed ", itf.data_o);
+    $display ("--------------");
+
+    @(tb_clk);
+    itf.yumi <= 1'b0;
+
+endtask : dequeue
+
+/* enqueue then dequeue 255 elements */
+task both_queues(i);
+    /* enqeuue the item */
+    @(tb_clk);
+    itf.valid_i <= 1'b1;
+    itf.data_i <= i;
+
+    @(tb_clk);
+    itf.yumi <= 1'b1;
+
+    @(tb_clk);
+    itf.yumi <= 1'b0;
+    itf.valid_i <= 1'b0;
+
+endtask : both_queues
+
+
+/* test reset */
+task test_reset();
+    @(tb_clk);
+
+    reset();
+
+    assert(itf.rdy == 1)
+    else begin
+        $error ("%0d: %0t: RESET_DOES_NOT_CAUSE_READY_O error detected", `__LINE__, $time);
+        report_error (RESET_DOES_NOT_CAUSE_READY_O);
+    end  
+endtask : test_reset
+
+
 initial begin
     reset();
     /************************ Your Code Here ***********************/
     // Feel free to make helper tasks / functions, initial / always blocks, etc.
 
+    /* enqueue 256 items */
+    for (int i = 0; i < 256; i++) begin
+        $display ("--------------");
+        $display ("Enqueueing: %d", i);
+        enqueue(i);
+    end
 
+    /* dequeue those items */
+    for (int j = 0; j < 256; j++) begin
+        $display ("--------------");
+        $display ("Dequeueing: %d", j);
+        dequeue(j);
+    end
+
+    /* enequeue/dequeue */
+    for (int i = 0; i < 255; i++) begin
+        both_queues(i);
+    end
+    
+    /* test reset */
+    test_reset();
+    
     /***************************************************************/
     // Make sure your test bench exits by calling itf.finish();
     itf.finish();
